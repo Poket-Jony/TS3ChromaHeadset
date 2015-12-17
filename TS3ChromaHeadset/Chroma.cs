d38bfb10db6bd588662ab4dc75ba08cd948277dc
jonasp <jonasp@six.de>
2015-12-17 09:46:49 +0100

    fixed timer added debug

--- TS3ChromaHeadset/Chroma.cs
+++ TS3ChromaHeadset/Chroma.cs
@@ -6,16 +6,18 @@ using System.Threading.Tasks;
 using Corale.Colore.Core;
 using Corale.Colore.Razer.Headset;
 using System.Windows.Forms;
+using System.Timers;
 
 namespace TS3ChromaHeadset
 {
     internal static class Chroma
     {
+        private static bool Debug { get; set; } = false;
         private const int colorTimerInterval = 10000;
 
         internal static System.Drawing.Color CurrentColor { get; set; } = System.Drawing.Color.White;
-        private static bool isColorTimerTickInit = false;
-        private static Timer colorTimer = new Timer();
+        private static bool isColorTimerElapsedInit = false;
+        private static System.Timers.Timer colorTimer = new System.Timers.Timer();
 
         internal static void UpdateChromaColor(System.Drawing.Color color)
         {
@@ -27,7 +29,10 @@ namespace TS3ChromaHeadset
             if (colorTimer.Enabled)
                 colorTimer.Stop();
             CurrentColor = color;
-            Headset.Instance.SetBreathing(Corale.Colore.Core.Color.FromSystemColor(CurrentColor));
+            if (Debug)
+                MessageBox.Show("SetBreathing()");
+            else
+                Headset.Instance.SetBreathing(Corale.Colore.Core.Color.FromSystemColor(CurrentColor));
             InitColorTimer(interval);
             colorTimer.Start();
         }
@@ -35,17 +40,20 @@ namespace TS3ChromaHeadset
         private static void InitColorTimer(int interval)
         {
             colorTimer.Interval = interval;
-            if (!isColorTimerTickInit)
+            if (!isColorTimerElapsedInit)
             {
-                colorTimer.Tick += ColorTimer_Tick;
-                isColorTimerTickInit = true;
+                colorTimer.Elapsed += ColorTimer_Elapsed;
+                isColorTimerElapsedInit = true;
             }
         }
 
-        private static void ColorTimer_Tick(object sender, EventArgs e)
+        private static void ColorTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
             colorTimer.Stop();
-            Headset.Instance.Clear();
+            if (Debug)
+                MessageBox.Show("Clear()");
+            else
+                Headset.Instance.Clear();
         }
     }
 }
